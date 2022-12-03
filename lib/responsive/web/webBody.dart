bc71b18df51e17b7747ea9807a78f974ecaa1035
ShallowFeather <74778927+ShallowFeather@users.noreply.github.com>
2022-12-03 23:40:57 +0800

    Dynamic change screen

--- lib/responsive/web/webBody.dart
+++ lib/responsive/web/webBody.dart
@@ -42,40 +42,26 @@ class _WebBody extends State<WebBody> {
                 ])
               ],
             ),
-            Row(children: [
-              Container(
-                width: 300,
-                height: 150,
-                padding: EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                  color: Colors.deepPurple[300],
-                  borderRadius: BorderRadius.circular(16),
-                ),
-                child: Container(),
-              ),
-              SizedBox(width: 50),
-              Container(
-                width: 300,
-                height: 150,
-                padding: EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                  color: Colors.lightBlueAccent,
-                  borderRadius: BorderRadius.circular(16),
-                ),
-                child: Container(),
-              ),
-              SizedBox(width: 50),
-              Container(
-                width: 300,
-                height: 150,
-                padding: EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                  color: Colors.redAccent,
-                  borderRadius: BorderRadius.circular(16),
-                ),
-                child: Container(),
-              )
-            ]),
+            Container(
+                padding: EdgeInsets.only(left: 50, top: 20, right: 30),
+                height: 175,
+                child: LayoutBuilder(builder: (context, constraints) {
+                  return ListView.separated(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: (constraints.maxWidth / 300).toInt(),
+                      itemBuilder: (BuildContext context, int index) {
+                        return Container(
+                          width: 275,
+                          decoration: BoxDecoration(
+                            color: Colors.deepPurple[300],
+                            borderRadius: BorderRadius.circular(16),
+                          ),
+                          child: Container(),
+                        );
+                      },
+                      separatorBuilder: (BuildContext context, int index) =>
+                          SizedBox(width: 50));
+                })),
             SizedBox(height: 30),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -102,30 +88,24 @@ class _WebBody extends State<WebBody> {
                 ])
               ],
             ),
-            ProblemList(),
+            Expanded(child: LayoutBuilder(builder: (context, constraints) {
+              return ListView.separated(
+                padding: EdgeInsets.only(right: 50),
+                shrinkWrap: true,
+                itemCount: (constraints.maxHeight / 65).toInt(),
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    leading: Icon(Icons.add_to_home_screen),
+                    title: Text("題目名稱"),
+                    trailing: Icon(Icons.keyboard_arrow_right),
+                  );
+                },
+                separatorBuilder: (BuildContext context, int index) {
+                  return Divider(color: Colors.blue);
+                },
+              );
+            })),
           ])),
-    );
-  }
-}
-
-
-class ProblemList extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return ListView.separated(
-      padding: EdgeInsets.only(right: 50),
-      shrinkWrap: true,
-      itemCount: 6,
-      itemBuilder: (BuildContext context, int index) {
-        return ListTile(
-            leading: Icon(Icons.add_to_home_screen),
-            title: Text("題目名稱"),
-            trailing: Icon(Icons.keyboard_arrow_right),
-        );
-      },
-      separatorBuilder: (BuildContext context, int index) {
-        return Divider(color: Colors.blue);
-      },
     );
   }
 }
