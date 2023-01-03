cc27634400c383f086c6d20f367e823853d0223c
ShallowFeather <74778927+ShallowFeather@users.noreply.github.com>
2023-01-04 01:09:57 +0800

    小改動

--- lib/responsive/web/note/noteBody.dart
+++ lib/responsive/web/note/noteBody.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:judgebox/constants.dart';
 
@@ -9,60 +7,132 @@ class NoteBody extends StatefulWidget {
   @override
   State<NoteBody> createState() => _NoteBody();
 }
+
+bool _TypeDraw = false;
+
 //文字方塊
 class _NoteBody extends State<NoteBody> {
+  final buttonKey = GlobalKey();
+  final textBoxKey = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
+        body: Stack(children: <Widget>[
+      Column(children: [
+        IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {
+            setState(() {
+              _TypeDraw = !_TypeDraw;
+            });
+          },
+        ),
+      ]),
+      Container(
           padding: EdgeInsets.only(left: 250, right: 250),
-          child: Column(children: [
-            Expanded(child: LayoutBuilder(builder: (context, constraints) {
-              return CustomPaint(
-                painter: PointPainter(),
-                child: Container(
-                  child: GestureDetector(
-                    onPanDown: (details) {
-                      // Handle the user's mouse down event
-                    },
-                    onPanUpdate: (details) {
-                      RenderBox? box = context.findRenderObject() as RenderBox;
-                      PointPainter.points.add(box.globalToLocal(details.globalPosition));
-                      setState(() {});
-                    },
-                    child: Container(
-                      // Use a Container widget to set the background color and size of the text field
-                      color: Colors.blue,
-                      width: 300,
-                      height: 200,
-                      child: TextField(
-                        maxLines: 3, // This will create a multi-line text field with 3 lines
-                      ),
+          child: _TypeDraw
+              ? Stack(children: [
+                  Container(
+                    padding: EdgeInsets.only(top: 20),
+                    child: ListView.builder(
+                      itemCount: 10,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          width: double.infinity,
+                          child: TextField(),
+                        );
+                      },
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(10),
+                          topRight: Radius.circular(10),
+                          bottomLeft: Radius.circular(10),
+                          bottomRight: Radius.circular(10)),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset: Offset(0, 3), // changes position of shadow
+                        ),
+                      ],
                     ),
                   )
-                  ,
-                ),
-              );
-            })),
-          ])),
+                ])
+              : Stack(children: [
+                  Container(
+                    child: Text("ouo"),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(10),
+                          topRight: Radius.circular(10),
+                          bottomLeft: Radius.circular(10),
+                          bottomRight: Radius.circular(10)),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset: Offset(0, 3), // changes position of shadow
+                        ),
+                      ],
+                    ),
+                  )
+                ]))
+    ]));
+  }
+}
+
+class TextBoxContainer extends StatefulWidget {
+  TextBoxContainer({required Key key}) : super(key: key);
+
+  @override
+  _TextBoxContainerState createState() => _TextBoxContainerState();
+}
+
+class _TextBoxContainerState extends State<TextBoxContainer> {
+  // Use a List to store the positions of the text boxes
+  List<Offset> textBoxPositions = [];
+
+  void createTextBox(Offset position) {
+    // Add the position to the list
+    setState(() {
+      textBoxPositions.add(position);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // Use the Stack widget to overlay the text boxes on top of each other
+      child: Stack(
+        children: textBoxPositions.map((position) {
+          return Positioned(
+            // Use the position to position the text box on the screen
+            left: position.dx,
+            top: position.dy,
+            child: TextBox(),
+          );
+        }).toList(),
+      ),
     );
   }
 }
 
-class PointPainter extends CustomPainter {
-  static List<Offset> points = <Offset>[];
-
+class TextBox extends StatelessWidget {
   @override
-  void paint(Canvas canvas, Size size) {
-    var paint = Paint()
-      ..color = Colors.red
-      ..strokeWidth = 20.0
-      ..strokeCap = StrokeCap.round;
-    for (int i = 0; i < points.length; i++) {
-      if (points[i] != null) canvas.drawPoints(PointMode.points, [points[i]], paint);
-    }
+  Widget build(BuildContext context) {
+    return Container(
+      width: 200,
+      height: 100,
+      decoration: BoxDecoration(
+        border: Border.all(),
+      ),
+      child: TextField(),
+    );
   }
-
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) => true;
 }
