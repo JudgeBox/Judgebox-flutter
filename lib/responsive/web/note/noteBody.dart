3f5e721a7130046e9448e9fafe82c38613fb421d
ShallowFeather <74778927+ShallowFeather@users.noreply.github.com>
2023-02-16 21:48:08 +0800

    Has some problem with Add button it need to be add a note in pages but looks unwork :(

--- lib/responsive/web/note/noteBody.dart
+++ lib/responsive/web/note/noteBody.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:judgebox/constants.dart';
 
 class NoteBody extends StatefulWidget {
@@ -8,131 +11,208 @@ class NoteBody extends StatefulWidget {
   State<NoteBody> createState() => _NoteBody();
 }
 
-bool _TypeDraw = false;
-
-//文字方塊
 class _NoteBody extends State<NoteBody> {
   final buttonKey = GlobalKey();
   final textBoxKey = GlobalKey();
+  bool _isTop = true;
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: Stack(children: <Widget>[
-      Column(children: [
-        IconButton(
-          icon: Icon(Icons.menu),
-          onPressed: () {
-            setState(() {
-              _TypeDraw = !_TypeDraw;
-            });
-          },
-        ),
-      ]),
-      Container(
-          padding: EdgeInsets.only(left: 250, right: 250),
-          child: _TypeDraw
-              ? Stack(children: [
-                  Container(
-                    padding: EdgeInsets.only(top: 20),
-                    child: ListView.builder(
-                      itemCount: 10,
-                      itemBuilder: (context, index) {
-                        return Container(
-                          width: double.infinity,
-                          child: TextField(),
-                        );
-                      },
-                    ),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(10),
-                          topRight: Radius.circular(10),
-                          bottomLeft: Radius.circular(10),
-                          bottomRight: Radius.circular(10)),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.5),
-                          spreadRadius: 5,
-                          blurRadius: 7,
-                          offset: Offset(0, 3), // changes position of shadow
-                        ),
-                      ],
-                    ),
-                  )
-                ])
-              : Stack(children: [
-                  Container(
-                    child: Text("ouo"),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(10),
-                          topRight: Radius.circular(10),
-                          bottomLeft: Radius.circular(10),
-                          bottomRight: Radius.circular(10)),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.5),
-                          spreadRadius: 5,
-                          blurRadius: 7,
-                          offset: Offset(0, 3), // changes position of shadow
-                        ),
-                      ],
-                    ),
-                  )
-                ]))
-    ]));
-  }
-}
-
-class TextBoxContainer extends StatefulWidget {
-  TextBoxContainer({required Key key}) : super(key: key);
-
-  @override
-  _TextBoxContainerState createState() => _TextBoxContainerState();
-}
-
-class _TextBoxContainerState extends State<TextBoxContainer> {
-  // Use a List to store the positions of the text boxes
-  List<Offset> textBoxPositions = [];
-
-  void createTextBox(Offset position) {
-    // Add the position to the list
+  void _changeOrder() {
     setState(() {
-      textBoxPositions.add(position);
+      _isTop = !_isTop;
     });
   }
 
+  late List<FocusNode> textFocusNode;
+
+  late List<List<Widget>> pages;
+
+  @override
+  void initState() {
+    super.initState();
+    // Initialize pages with a single page.
+    pages = [
+      [
+        PainterContainer(),
+        TextContainer(),
+      ],
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      // Use the Stack widget to overlay the text boxes on top of each other
-      child: Stack(
-        children: textBoxPositions.map((position) {
-          return Positioned(
-            // Use the position to position the text box on the screen
-            left: position.dx,
-            top: position.dy,
-            child: TextBox(),
-          );
-        }).toList(),
+    List<Widget> notes = [
+      PainterContainer(),
+      TextContainer(),
+    ];
+    // if (!_isTop) {
+    //   for(var i = 0; i < page.length; i++) {
+    //     page[i] = page[i].reversed.toList();
+    //   }
+    //   //notes = notes.reversed.toList();
+    // }
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Expanded(
+            child: PageView.builder(
+              itemCount: pages.length,
+              itemBuilder: (context, index) {
+                return Container(
+                  padding: EdgeInsets.only(left: 250, right: 250),
+                  color: Colors.white,
+                  child: Stack(
+                    children: pages[index],
+                  ),
+                );
+              },
+            ),
+          ),
+          Column(
+            children: [
+              Column(
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.menu),
+                    onPressed: () {
+                      setState(() {
+                        pages = pages
+                            .map((page) => page.reversed.toList())
+                            .toList();
+                      });
+                    },
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.add),
+                    onPressed: () {
+                      setState(() {
+                        // Add a new page to the end of the list.
+                        print(pages.length);
+                        pages.add([
+                          PainterContainer(),
+                          TextContainer(),
+                        ]);
+                      });
+                    },
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
 }
 
-class TextBox extends StatelessWidget {
+class PainterContainer extends StatefulWidget {
+  const PainterContainer({Key? key}) : super(key: key);
+
+  @override
+  State<PainterContainer> createState() => _PainterContainer();
+}
+
+class _PainterContainer extends State<PainterContainer> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 200,
-      height: 100,
-      decoration: BoxDecoration(
-        border: Border.all(),
+        child: CustomPaint(
+      painter: PointPainter(),
+      child: Container(
+        child: GestureDetector(
+          onPanUpdate: (details) {
+            RenderBox? box = context.findRenderObject() as RenderBox;
+            PointPainter.points.add(box.globalToLocal(details.globalPosition));
+            setState(() {});
+          },
+        ),
       ),
-      child: TextField(),
-    );
+    ));
+  }
+}
+
+class PointPainter extends CustomPainter {
+  static List<Offset> points = <Offset>[];
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint()
+      ..color = Colors.red
+      ..strokeWidth = 4.0
+      ..strokeCap = StrokeCap.round;
+    for (int i = 0; i < points.length; i++) {
+      if (points[i] != null)
+        canvas.drawPoints(PointMode.points, [points[i]], paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
+}
+
+class TextContainer extends StatefulWidget {
+  const TextContainer({Key? key}) : super(key: key);
+
+  @override
+  State<TextContainer> createState() => _TextContainer();
+}
+
+class _TextContainer extends State<TextContainer> {
+  @override
+  Widget build(BuildContext context) {
+    int listLength = 10;
+    List<FocusNode> _focusNodes =
+        List<FocusNode>.generate(listLength, (int index) => FocusNode());
+
+    List<Widget> textFields = List<Widget>.generate(
+        listLength,
+        (int index) => RawKeyboardListener(
+            focusNode: FocusNode(),
+            onKey: (event) {
+              if (event is RawKeyDownEvent) {
+                if (event.logicalKey == LogicalKeyboardKey.arrowUp) {
+                  // 处理键盘上的向上箭头按键
+                  _focusNodes[index - 1].requestFocus();
+                } else if (event.logicalKey == LogicalKeyboardKey.arrowDown) {
+                  // 处理键盘上的向下箭头按键
+                  _focusNodes[index + 1].requestFocus();
+                }
+              }
+            },
+            child: TextField(
+              controller: TextEditingController(),
+              focusNode: _focusNodes[index],
+              onSubmitted: (String value) {
+                if (index < listLength) {
+                  _focusNodes[index + 1].requestFocus();
+                }
+              },
+              onChanged: (value) {
+                if (value.contains('\n')) {
+                  // 检测到换行符时，将焦点移动到下一个TextField
+                  _focusNodes[index + 1].requestFocus();
+                }
+              },
+            )));
+
+    return Container(
+        padding: EdgeInsets.only(top: 20),
+        decoration: BoxDecoration(
+          color: Colors.transparent,
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(10),
+              topRight: Radius.circular(10),
+              bottomLeft: Radius.circular(10),
+              bottomRight: Radius.circular(10)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.white.withOpacity(0.5),
+              spreadRadius: 5,
+              blurRadius: 7,
+              offset: Offset(0, 3), // changes position of shadow
+            ),
+          ],
+        ),
+        child:
+            ListView(padding: const EdgeInsets.all(8.0), children: textFields));
   }
 }
